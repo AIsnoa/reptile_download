94e8b7f06263a2d1231c1d5f37151749b707d903
lianjie <812590783@qq.com>
2023-02-10 00:03:52 +0800

    输出优化

--- video/reptile.php
+++ video/reptile.php
@@ -1,37 +1,51 @@
 <?php
 
-	$handle = fopen("downland_list_data.txt",'a+');
+	$handle = fopen("download_list.txt",'a+');
 	$ret = fwrite($handle,"七龙珠".PHP_EOL);
 	if(!$ret){
 		echo $data."write fail!";
 	}	
-	fclose($handle);	
-	$episode = 5;
+	$episode = 2;
+	
 	while($episode <= 153){
+		$time1 = microtime(true);
+
 		$address_number = $episode -1;
-		$url = "https://www.guaguo.cc/a/122065-0-".strval($address_number).".html";    //第5集的地址
+		$url = "https://www.guaguo.cc/a/122065-0-".strval($address_number).".html";    //第1集的地址
 		$pattern = "/<script>.*.<\/script>/";
 		$result = [];
 		$content = file_get_contents($url);
+		echo $content;
+	break;	
 		$ret = preg_match_all($pattern,$content,$result);
+
+
 		//echo $content.PHP_EOL;
-		//print_r($result);
+		print_r($result);
 		$key_script = $result[0][2];
 		$temp1 = strstr($key_script,'now="');
+		
 		$temp2 = strstr($temp1,';',true);
 		//echo PHP_EOL.$temp2;
+	
 		$temp3 = strstr($temp2,'h');
+	
 		$key_info = strstr($temp3,'"',true);
-		//echo PHP_EOL.$key_info;
+		//echo PHP_EOL.$key_info;	
+		
 		$data = "第".$episode."集地址: ".$key_info.PHP_EOL;	
-		$handle = fopen("downland_list_data.txt",'a+');
 		$ret = fwrite($handle,$data);
 		if(!$ret){
-			echo $data."write fail!";
+		echo $data."write fail!";
 		}	
-		fclose($handle);	
-		echo "finish ".strval($episode)." address write: ".$data.PHP_EOL;
+		echo "finish ".$data.PHP_EOL;
 		$episode++;
+		
+		$time2 = microtime(true);
+                $use_time = $time2-$time1;
+                $use_time = round($use_time,3);
+                echo "Finish analysis.".' use time:'.strval($use_time).'s.'.PHP_EOL;
 	}
+		fclose($handle);	
 
 ?>
