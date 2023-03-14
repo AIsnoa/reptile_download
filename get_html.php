e6233dcddd02142b360339e32e9e6464543d8f2e
lianjie <812590783@qq.com>
2023-03-14 23:55:00 +0800

    curl shell

--- get_html.php
+++ get_html.php
@@ -1,5 +1,6 @@
 <?php
-	$url = "https://music.163.com/#/song?id=1897100445";
-	$html = file_get_contents($url);
-	echo $html.PHP_EOL;
+	$url = "https://www.guaguo.cc/a/122065-0-0.html";
+//	$html = file_get_contents($url); //http
+	$curl_shell = "curl ".$url;
+	$data = shell_exec($curl_shell);
 ?>
